0dd476ffc867e05fcdf1ec5ebfd3084556ad32e2
innaslota <innaslota8@gmail.com>
2023-04-17 20:48:16 +0300

    feat: resolve mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,10 +23,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
+  const result = [];
+
+  // Initialize result matrix with zeros
+  for (let i = 0; i < numRows; i++) {
+    result[i] = Array(numCols).fill(0);
+  }
+
+  // Iterate over each cell in the matrix
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+      if (matrix[i][j]) {
+        // If cell is a mine, set corresponding cell in result matrix to -1
+        result[i][j] = -1;
+      } else {
+        // Otherwise, count number of neighboring mines
+        let count = 0;
+        for (let x = i - 1; x <= i + 1; x++) {
+          for (let y = j - 1; y <= j + 1; y++) {
+            if (x >= 0 && x < numRows && y >= 0 && y < numCols && matrix[x][y]) {
+              count++;
+            }
+          }
+        }
+        result[i][j] = count;
+      }
+    }
+  }
+
+  return result;
 }
+
 
 module.exports = {
   minesweeper
