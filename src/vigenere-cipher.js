be159666fd501a3789036cd22846f59f1198c8d8
innaslota <innaslota8@gmail.com>
2023-04-17 20:30:52 +0300

    feat: resolve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,59 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse = true) {
+    this.reverse = reverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase().replace(/[^A-Z]/g, '');
+    key = key.toUpperCase().replace(/[^A-Z]/g, '');
+
+    let encryptedMessage = '';
+    let j = 0;
+    for (let i = 0; i < message.length; i++) {
+      let messageCharCode = message.charCodeAt(i);
+      let keyCharCode = key.charCodeAt(j % key.length);
+      let shift = keyCharCode - 65;
+      let encryptedCharCode = ((messageCharCode + shift - 65) % 26) + 65;
+      encryptedMessage += String.fromCharCode(encryptedCharCode);
+      if (messageCharCode >= 65 && messageCharCode <= 90) {
+        j++;
+      }
+    }
+
+    return this.reverse ? encryptedMessage : encryptedMessage.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    encryptedMessage = encryptedMessage.toUpperCase().replace(/[^A-Z]/g, '');
+    key = key.toUpperCase().replace(/[^A-Z]/g, '');
+
+    let decryptedMessage = '';
+    let j = 0;
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      let encryptedCharCode = encryptedMessage.charCodeAt(i);
+      let keyCharCode = key.charCodeAt(j % key.length);
+      let shift = keyCharCode - 65;
+      let decryptedCharCode = ((encryptedCharCode - shift - 65 + 26) % 26) + 65;
+      decryptedMessage += String.fromCharCode(decryptedCharCode);
+      if (encryptedCharCode >= 65 && encryptedCharCode <= 90) {
+        j++;
+      }
+    }
+
+    return this.reverse ? decryptedMessage : decryptedMessage.split('').reverse().join('');
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
