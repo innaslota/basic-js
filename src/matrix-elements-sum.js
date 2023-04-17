66899a3bad9da895d821843192f3bd74314a9e9f
innaslota <innaslota8@gmail.com>
2023-04-17 20:46:29 +0300

    feat: resolve matrix-elements-sum

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,10 +16,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const zeroRows = new Array(cols).fill(false); // initialize zeroRows to all false values
+  let sum = 0;
+
+  for (let j = 0; j < cols; j++) {
+    for (let i = 0; i < rows; i++) {
+      if (matrix[i][j] === 0) {
+        zeroRows[j] = true; // mark this column as having a zero value
+        break; // move on to the next column
+      } else if (!zeroRows[j]) {
+        sum += matrix[i][j]; // add this value to the sum if we haven't seen a zero in this column yet
+      }
+    }
+  }
+
+  return sum;
 }
+
 
 module.exports = {
   getMatrixElementsSum
