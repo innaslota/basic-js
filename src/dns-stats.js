290dc6916bbe9726fc23ab2eaead3d9df6a97656
innaslota <innaslota8@gmail.com>
2023-04-17 20:36:59 +0300

    feat: resolve dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,10 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const dnsStats = {};
+
+  for (const domain of domains) {
+    const parts = domain.split('.').reverse();
+    let subdomain = '';
+    
+    for (const part of parts) {
+      subdomain = `${part}${subdomain ? '.' : ''}${subdomain}`;
+      dnsStats[`.${subdomain}`] = (dnsStats[`.${subdomain}`] || 0) + 1;
+    }
+  }
+
+  return dnsStats;
 }
+
 
 module.exports = {
   getDNSStats
